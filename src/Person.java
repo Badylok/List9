fa2641ea92d2c4c60c9668db832bb9cf87d96bea
milosz <badylakmilosz@gmail.com>
2025-12-10 16:01:55 +0100

    Person

--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,14 @@
-class Person {
+// I define the abstract base class Person containing common attributes
+abstract class Person {
+    protected String name;
+    protected int age;
+
+    Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    void introduce() {
+        System.out.println("Hi, my name is " + name + " and I am " + age + " years old.");
+    }
 }
